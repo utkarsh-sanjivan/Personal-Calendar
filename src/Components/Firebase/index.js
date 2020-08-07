0d57fb8707e1411f040f71662235d78dea9686e8
utkarsh-sanjivan <68389878+utkarsh-sanjivan@users.noreply.github.com>
2020-08-07 15:52:42 +0530

    Final Skeleton (#2)

--- src/Components/Firebase/index.js
+++ src/Components/Firebase/index.js
@@ -4,9 +4,6 @@ import React from 'react';
 
 export default function withFirebase(OriginalComponent) {
     return class extends React.Component {
-        constructor(props){
-            super(props);
-        }
         render() {
             return <OriginalComponent {...this.props} firebase={firebase}/>
         }
